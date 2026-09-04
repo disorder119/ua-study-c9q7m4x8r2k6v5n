7421949808e9755c78cf16b195c851421ad34c73
disorder119 <joelb119@googlemail.com>
2026-09-05 00:02:36 +0200

    Aktualisiere Offline-Cache auf Kurs v10

--- ukrainisch-lernen-sw.js
+++ ukrainisch-lernen-sw.js
@@ -1,4 +1,4 @@
-const CACHE='ukrainischkurs-joel-v9';
+const CACHE='ukrainischkurs-joel-v10';
 const ASSETS=['./','./index.html','./ukrainischkurs-app.html','./ukrainisch-lernen.html','./ukrainischkurs-v2-loader.js','./ukrainischkurs-v2.part1','./ukrainischkurs-v2.part2','./ukrainischkurs-v2.part3','./ukrainischkurs-v2.part4','./ukrainischkurs-v2.part5','./ukrainischkurs-native-audio.js','./ukrainischkurs-pronunciation.js','./ukrainischkurs-pronunciation-mastery.js','./ukrainischkurs-quality-hardening.js','./ukrainischkurs-selftest.js','./ukrainisch-lernen.webmanifest','./ukrainisch-lernen-icon.svg','./ukrainisch-icon-180.png','./ukrainisch-icon-192.png','./ukrainisch-icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
