7432d5308ba85b7bad2e84b5bfa5aacdc5717f8e
disorder119 <joelb119@googlemail.com>
2026-09-04 23:34:00 +0200

    Lade Aussprache-Coach nach Kurslogik

--- ukrainischkurs-v2-loader.js
+++ ukrainischkurs-v2-loader.js
@@ -1,10 +1,14 @@
 (async()=>{
   try{
-    const urls=[1,2,3,4,5].map(n=>`./ukrainischkurs-v2.part${n}?v=6`);
+    const urls=[1,2,3,4,5].map(n=>`./ukrainischkurs-v2.part${n}?v=7`);
     const responses=await Promise.all(urls.map(url=>fetch(url,{cache:'no-store'})));
     if(responses.some(response=>!response.ok))throw new Error('Upgrade-Teile fehlen');
     const code=(await Promise.all(responses.map(response=>response.text()))).join('');
     eval(code);
+
+    const pronunciationResponse=await fetch('./ukrainischkurs-pronunciation.js?v=1',{cache:'no-store'});
+    if(!pronunciationResponse.ok)throw new Error('Aussprache-Coach fehlt');
+    eval(await pronunciationResponse.text());
   }catch(error){
     console.error('Ukrainischkurs-Upgrade konnte nicht geladen werden',error);
     const toast=document.getElementById('toast');
