d7f6f569dce05e66be47af666d5384eb254720a6
disorder119 <joelb119@googlemail.com>
2026-09-05 12:27:51 +0200

    Aktualisiere Offline-Cache auf v38

--- ukrainisch-lernen-sw.js
+++ ukrainisch-lernen-sw.js
@@ -1,4 +1,4 @@
-const CACHE='ukrainischkurs-joel-v37';
+const CACHE='ukrainischkurs-joel-v38';
 const ASSETS=[
   './','./index.html','./ukrainischkurs-app.html','./ukrainisch-lernen.html','./ukrainisch-lernen.webmanifest',
   './ukrainisch-icon-180.png','./ukrainisch-icon-192.png','./ukrainisch-icon-512.png','./ukrainisch-lernen-icon.svg',
