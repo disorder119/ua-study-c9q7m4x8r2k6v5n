1e218ed7ed0db71b1b76145d6d54504df709aacf
disorder119 <joelb119@googlemail.com>
2026-09-13 21:20:22 +0200

    Add V4 browser app initializer

---
+++ alphabet-app-v4-init.js
@@ -0,0 +1,3 @@
+'use strict';
+window.AlphabetLab={version:C.SCHEMA_VERSION||C.VERSION,state:()=>structuredClone(S),export:()=>JSON.stringify(S,null,2),reset(){if(confirm('Alphabet-Lernstand wirklich löschen?')){[STORAGE,LEGACY2,LEGACY1].forEach(k=>localStorage.removeItem(k));S=C.freshState();persist();screen='home';render()}},startExam,startMyTraining};
+render();
