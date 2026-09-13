7d86c22a95e9bc83d91988e73f4c9dfc78305732
disorder119 <joelb119@googlemail.com>
2026-09-13 21:19:43 +0200

    Activate V4 adaptive core with V3 test fallback

--- alphabet-core-v2.js
+++ alphabet-core-v2.js
@@ -1,2 +1,21 @@
 'use strict';
-globalThis.AlphabetCoreV2=Object.freeze({VERSION,DAY,HOUR,MIN,ALPHABET,LEARN_ORDER,FAKE_FRIENDS,LATIN_TRAPS,HARD,VOWELS,CONSONANTS,SPECIAL,DATA,SOUND_GROUPS,CONTRASTS,CORE_SKILLS,SKILLS,KIND_SKILL,SKILL_LABELS,INTERVALS,uniq,clamp,shuffle,id,localDateKey,todayFrom,daysBetween,skillForKind,freshState,migrate,touchStudy,recentAccuracy,independentAccuracy,avgSkillLatency,skillMastery,coreSkillScores,retentionDaysForLetter,letterMastery,letterReady,letterStatus,problemFlag,recordAnswer,recordWriting,topConfusions,errorPriority,weakLetters,errorLetters,dueSkillPairs,dueLetters,secureLetters,buildDistractors,makeTask,variantizeTask,buildExam,repairTask,buildDiagnostic,buildCertification,buildAudioCertification,buildFakeCertification,buildFinalCertification,recordExam,addMasteryCheck,averageRecognitionMs,mastered,statsToday,summaryForLetter,openMasteryItems,createExamSession,scheduleRepairForSession,dueRepairForSession,applyMainAnswer,applyRepairAnswer,sessionScore,toggleBookmark,isBookmarked,taskFromBookmark,plausibleSyllable});
+if(typeof V4_VERSION!=='undefined'){
+  globalThis.AlphabetCoreV2=Object.freeze({
+    VERSION:3,SCHEMA_VERSION:V4_VERSION,DAY,HOUR,MIN,ALPHABET,LEARN_ORDER,FAKE_FRIENDS,LATIN_TRAPS,HARD,VOWELS,CONSONANTS,SPECIAL,DATA,SOUND_GROUPS,CONTRASTS,CORE_SKILLS,SKILLS,KIND_SKILL,SKILL_LABELS,INTERVALS,
+    WORD_BANK,LETTER_PEDAGOGY,FONT_VARIANTS,QUESTION_FAMILIES,LEARNING_STATE_LABELS,
+    uniq,clamp,shuffle,id,localDateKey,todayFrom,daysBetween,skillForKind,
+    freshState:freshStateV4,migrate:migrateV4,touchStudy,recentAccuracy,independentAccuracy,avgSkillLatency,
+    skillMastery:skillMasteryV4,skillConfidence,coreSkillScores:coreSkillScoresV4,retentionDaysForLetter,letterMastery:letterMasteryV4,
+    letterReady:letterReadyV4,letterStatus:letterStatusV4,learningState,problemFlag,
+    recordAnswer:recordAnswerV4,recordWriting,topConfusions,errorPriority,weakLetters,errorLetters,dueSkillPairs,dueLetters,secureLetters:secureLettersV4,
+    calculateLearningNeed,shouldUnlockNextLetter,recomputeLearningPlan,activeLearningSet,reviewSetFor,difficultyFor,wordsForLetter,pickWord,familyCandidates,
+    buildDistractors,makeTask,variantizeTask,buildExam,buildV4Task,questionSignature,noveltyScore,noteExposure,detectLatinTrap,
+    selectNextMainQuestion,selectLetterV4,selectSkillV4,selectFamilyV4,overexposurePenalty,createAdaptiveSession,createFocusedSession,createBattleSession,sessionQualityMetrics,sessionDelta,
+    repairTask:repairTaskV4,registerRepair,openRepairIds,syncRepairPending,scheduleRepairForSession:scheduleRepairForSessionV4,dueRepairForSession,applyMainAnswer,applyRepairAnswer,sessionScore,
+    buildDiagnostic,buildExamV4:buildRealExamV4,buildCertification:buildCertificationV4,buildAudioCertification,buildFakeCertification,buildFinalCertification:buildFinalCertificationV4,
+    recordExam,addMasteryCheck,averageRecognitionMs,mastered:masteredV4,statsToday,summaryForLetter:summaryForLetterV4,openMasteryItems:openMasteryItemsV4,
+    createExamSession,toggleBookmark,isBookmarked,taskFromBookmark,plausibleSyllable,technicalAudioFailureDecision,makeAudioFallback,snapshotMastery
+  });
+}else{
+  globalThis.AlphabetCoreV2=Object.freeze({VERSION,DAY,HOUR,MIN,ALPHABET,LEARN_ORDER,FAKE_FRIENDS,LATIN_TRAPS,HARD,VOWELS,CONSONANTS,SPECIAL,DATA,SOUND_GROUPS,CONTRASTS,CORE_SKILLS,SKILLS,KIND_SKILL,SKILL_LABELS,INTERVALS,uniq,clamp,shuffle,id,localDateKey,todayFrom,daysBetween,skillForKind,freshState,migrate,touchStudy,recentAccuracy,independentAccuracy,avgSkillLatency,skillMastery,coreSkillScores,retentionDaysForLetter,letterMastery,letterReady,letterStatus,problemFlag,recordAnswer,recordWriting,topConfusions,errorPriority,weakLetters,errorLetters,dueSkillPairs,dueLetters,secureLetters,buildDistractors,makeTask,variantizeTask,buildExam,repairTask,buildDiagnostic,buildCertification,buildAudioCertification,buildFakeCertification,buildFinalCertification,recordExam,addMasteryCheck,averageRecognitionMs,mastered,statsToday,summaryForLetter,openMasteryItems,createExamSession,scheduleRepairForSession,dueRepairForSession,applyMainAnswer,applyRepairAnswer,sessionScore,toggleBookmark,isBookmarked,taskFromBookmark,plausibleSyllable});
+}
